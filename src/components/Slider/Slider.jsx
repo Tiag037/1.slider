9f84e3def13d778379dfe2440ed841d2d1e0e01b
Tiag037 <d-tiago@live.fr>
2024-04-22 15:58:28 +0200

    Mise en place d'un slider automatique

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
 import "./Slider.css";
@@ -7,22 +7,42 @@ import sliderData from "../../data/sliderData";
 export default function Slider() {
   const [sliderIndex, setSliderIndex] = useState(1);
 
-  const toggleImage = (indexPilote) => {
+  const toggleImage = (indexPayload) => {
+    // 1ere maniere de faire, pour un changement manuel des images
+    /*
     let newState;
-    // si je suis à la fin et que je next, je retourner sur le permier index
-    if (indexPilote + sliderIndex > sliderData.length) {
-      newState = 1;
+      // si je suis à la fin et que je next, je retourner sur le permier index
+      if (indexPayload + sliderIndex > sliderData.length) {
+        newState = 1;
 
-      //si je suis au début et que je previous, je vais sur le dernier index
-    } else if (indexPilote + sliderIndex < 1) {
-      newState = sliderData.length;
+        //si je suis au début et que je previous, je vais sur le dernier index
+      } else if (indexPayload + sliderIndex < 1) {
+        newState = sliderData.length;
 
-      //si ce n'ai pas un cas de virgure comme ceux précécent, alors on avance ou recule normalement
-    } else {
-      newState = indexPilote + sliderIndex;
-    }
-    setSliderIndex(newState);
+        //si ce n'ai pas un cas de virgure comme ceux précécent, alors on avance ou recule normalement
+      } else {
+        newState = indexPayload + sliderIndex;
+      }
+      setSliderIndex(newState);
+      */
+
+    // pour rendre le défillement d'image (slider) en automatique
+    // on peut metre une fonction callback dans un usestate
+    setSliderIndex((state) => {
+      if (indexPayload + state > sliderData.length) {
+        return 1;
+      } else if (indexPayload + state < 1) {
+        return sliderData.length;
+      } else {
+        return state + indexPayload;
+      }
+    });
   };
+
+  useEffect(() => {
+    const intervalID = setInterval(() => toggleImage(1), 2000);
+    return () => clearInterval(intervalID)
+  }, []);
   return (
     <>
       <p className="index-info">
